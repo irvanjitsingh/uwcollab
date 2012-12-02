a19f91218681d09886b6937c697685450634b132
Nadayar Enegesi <nenegesi@gmail.com>
2012-12-02 04:31:01 -0500

    fuck i need sleep

--- uwcollab/auth/views.py
+++ uwcollab/auth/views.py
@@ -10,6 +10,9 @@ from django.views.generic.base import View
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
 from django.utils import simplejson as json
+from django.db.transaction import commit_on_success
+from django.views.generic.edit import FormView
+from forms import RegistrationForm
 
 
 def logout(request):
@@ -22,6 +25,27 @@ def home(request):
     user = request.user
     context = {'first_name': user.username}
     return render_to_response(template, context)
+
+
+class RegistrationView(FormView):
+    """
+    Form view to handle registration
+    """
+    form_class = RegistrationForm
+    success_url = "/"
+
+    @commit_on_success
+    def form_valid(self, form):
+        form.save()
+
+        user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+
+        # tasks.send_template_email.delay(settings.EMAIL_HOST_USER, [form.cleaned_data['email']],
+        #                             'registration/activation_email_subject.txt', {},
+        #                             'registration/activation_email.txt', {'user': user})
+        login(self.request, user)
+
+        return super(RegistrationView, self).form_valid(form)
 
 
 class LandingView(View):
