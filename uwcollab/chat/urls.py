86726be992dc97ef8acc44f8588b2ec71d3b27e0
Irvan <irvan.gill@gmail.com>
2012-12-02 04:40:17 -0500

    whiteboard

--- uwcollab/chat/urls.py
+++ uwcollab/chat/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls.defaults import patterns, include, url
 
 
 urlpatterns = patterns("chat.views",
-    url("^chat/$", "rooms", name="rooms"),
     url("^chat/create/$", "create", name="create"),
     url("^chat/system_message/$", "system_message", name="system_message"),
     url("^chat/(?P<slug>.*)$", "room", name="room"),
